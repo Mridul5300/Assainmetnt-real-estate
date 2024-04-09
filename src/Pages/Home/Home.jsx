258beee241c6f4bad846ab64b62b02064953b9ef
Mridul5300 <mridulsarkar1157@gmail.com>
2024-04-09 09:26:22 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+import Banner from './Bannar';
+
+const Home = () => {
+     return (
+          <div className='mt-52'>
+               <Banner></Banner>
+          </div>
+     );
+};
+
+export default Home;
---
+++ src/Root/Root.jsx
@@ -0,0 +1,19 @@
+import Footer from "../Pages/Footer/Footer"
+import Navbar from "../Pages/Navbar/Navbar";
+import { Outlet  } from "react-router-dom";
+
+
+const Root = () => {
+     return (
+          <div>
+               
+               <Navbar></Navbar>
+               <div>
+                    <Outlet></Outlet>
+               </div>
+               <Footer></Footer>
+          </div>
+     );
+};
+
+export default Root;
