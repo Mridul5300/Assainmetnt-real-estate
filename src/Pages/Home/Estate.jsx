ee4fbc2a35c5d793aa77971330ae80737c42862f
Mridul5300 <mridulsarkar1157@gmail.com>
2024-04-10 11:47:25 +0600

    half estat design done

--- src/Pages/Home/Estate.jsx
+++ src/Pages/Home/Estate.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const Estate = ({ statecard }) => {
-     const { estate_title } = statecard
+     const { estate_title,imageURL } = statecard
      return (
           <div data-aos="fade-up"
                data-aos-anchor-placement="center-bottom">
@@ -12,12 +12,12 @@ const Estate = ({ statecard }) => {
                          <div className='border p-4  shadow-2xl '>
                               <article className="flex flex-col dark:bg-gray-50">
                                    <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-                                        <img alt="" className="object-cover w-full h-60 dark:bg-gray-500" src="https://source.unsplash.com/200x200/?fashion?1" />
+                                        <img alt="" className="object-cover w-full h-60 dark:bg-gray-500" src={imageURL} />
                                    </a>
                                    <div className="flex flex-col flex-1">
                                         <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
                                         <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-600">Convenire</a>
-                                        <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">Te nulla oportere reprimique his dolorum</h3>
+                                        <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">{estate_title}</h3>
                                         <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-600">
                                              <span>June 1, 2020</span>
                                              <span>2.1K views</span>
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
      <li><NavLink to={"/login"}>Log in</NavLink></li>
      </>
      return (
-          <div className="navbar bg-base-100">
+          <div className="navbar border-b-4 mt-3 bg-base-100">
                <div className="navbar-start">
                     <div className="dropdown">
                          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
